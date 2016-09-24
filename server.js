871c11dc8776f4991d028dc5568c718473d647e7
Henry Sharber <hsharber@gmail.com>
2016-09-24 16:54:18 -0400

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -19,12 +20,8 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-    todos.forEach(function(item) {
-        if (item.id === todoId) {
-            matchedTodo = item;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
     if (matchedTodo) {
         res.json(matchedTodo);
     } else {
@@ -35,7 +32,13 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
     body.id = todoNextId;
     todos.push(body);
     todoNextId++;
